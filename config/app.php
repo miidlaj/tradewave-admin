826c2601a3a35b452c5e5d644a7978c75e751593
miidlaj <mumidlaj@gmail.com>
2024-01-28 15:54:26 +0530

    app url change

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://newtoktech.com'),
+    'url' => env('APP_URL', 'https://meet.newtoktech.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
